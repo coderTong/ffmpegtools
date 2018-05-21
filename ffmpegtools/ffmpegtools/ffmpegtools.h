bb58d4dc38a7adc4d70f0d723397688326c5a909
wutong <tong.wu@netviewtech.com>
2018-05-21 17:02:25 +0800

    Init

---
+++ ffmpegtools/ffmpegtools/ffmpegtools.h
@@ -0,0 +1,19 @@
+//
+//  ffmpegtools.h
+//  ffmpegtools
+//
+//  Created by codew on 2018/5/21.
+//  Copyright © 2018年 codew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ffmpegtools.
+FOUNDATION_EXPORT double ffmpegtoolsVersionNumber;
+
+//! Project version string for ffmpegtools.
+FOUNDATION_EXPORT const unsigned char ffmpegtoolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ffmpegtools/PublicHeader.h>
+
+
